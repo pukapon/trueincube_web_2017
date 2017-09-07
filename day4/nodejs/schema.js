6d225f0607f5f780db3a2eb359a3d8aafddf16a2
Thanat Maneenut <cadente.rd@gmail.com>
2017-09-07 16:03:01 +0700

    Create schema.js

---
+++ day4/nodejs/schema.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+const db = mongoose.connect('mongodb://localhost:27017/workshop_3', { useMongoClient: true });
+
+var topicSchema = mongoose.Schema({
+    name: String,
+    forum_id: { type: Schema.Types.ObjectId, ref: 'Forum'},
+    tags: [String],
+    comment_ids: [ {type: Schema.Types.ObjectId, ref: 'Comment' }],
+    user_id: { type: Schema.Types.ObjectId, ref: 'User' }
+});
+var userSchema = Schema({
+    name: String
+})
+var commentSchema = Schema({
+    name: String,
+    comment: String,
+    user_id: { type: Schema.Types.ObjectId, ref: 'User' }
+})
+var forumSchema = Schema({
+    name: String
+})
+var Topic = mongoose.model('Topic', topicSchema, 'topic');
+var User = mongoose.model('User', userSchema, 'user');
+var Comment = mongoose.model('Comment', userSchema, 'comment');
+var Forum = mongoose.model('Forum', userSchema, 'forum');
