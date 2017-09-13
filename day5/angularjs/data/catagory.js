6e220f6aea7056e08acad8896c84bf68d33de7ab
Suphat <switchimeen@gmail.com>
2017-09-14 01:53:59 +0700

    day5

---
+++ day5/angularjs/data/catagory.js
@@ -0,0 +1,21 @@
+var catagory = [{
+        id: 'Ramen',
+        desc: 'Ramen',
+        photo: "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649521_1499163965_ma.jpg"
+    },
+    {
+        id: 'Snacks',
+        desc: 'Snacks',
+        photo: "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649840_1499342394_ma.jpg"
+    },
+    {
+        id: 'Rice',
+        desc: 'Rice',
+        photo: "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649913_1499342385_ma.jpg"
+    },
+    {
+        id: 'Other',
+        desc: 'Other',
+        photo: "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649846_1499239874_ma.jpg"
+    }
+];
---
+++ day5/angularjs/data/japan_food.js
@@ -0,0 +1,182 @@
+var product = [
+    {
+      id: 1 ,
+      catagory:'Ramen',
+      name: "Uchidaya Ajitama Ramen",
+      detail: "เมนูซิกเนเจอร์รสชาติตามแบบฉบับโยโกฮาม่า เส้นราเมนกลมหนึบกำลังดีในน้ำซุปกระดูกหมูและนำมันไก่ ท้อปปิ้งด้วยหมูชาชู และไข่" ,
+      photo: "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649521_1499163965_ma.jpg",
+      qty: 60,
+      price: 250
+    },
+    {
+      id: 2 ,
+      catagory:'Ramen',
+      name: "Uchidaya Tokusei Ramen",
+      detail: "เมนูซิกเนเจอร์รสชาติตามแบบฉบับโยโกฮาม่า เส้นราเมนกลมหนึบกำลังดีในน้ำซุปกระดูกหมูและนำมันไก่ ท้อปปิ้งด้วยหมูชาชู ไข่ ผักโขม และสาหร่าย" ,
+      photo:"https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649523_1499163965_ma.jpg",
+      qty: 60,
+      price: 290
+    },
+    {
+      id: 3 ,
+      catagory:'Ramen',
+      name: "Maze Soba",
+      detail: "ราเมนแบบแห้งในซอสสูตรพิเศษของทางร้าน หอมกลิ่นปลาแห้งและน้ำมันงาแบบเผ็ด ท้อปปิ้งด้วยหมูชาชูหั่นเต๋า หัวหอมซอย สาหร่าย ไข่ออนเซ็น และสาหร่าย" ,
+      photo: "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649688_1499342425_ma.jpg",
+      qty: 60,
+      price: 240
+    },
+    {
+      id: 4 ,
+      catagory:'Ramen',
+      name: "Kyushu Tonkotsu Special",
+      qty: 40,
+      detail: "ราเมนเส้นเล็กในน้ำซุปกระดูกหมูเข้มข้น ท้อปปิ้งด้วยหมูชาชู ไข่ ผักดองปรุงรส และสาหร่าย" ,
+      photo: "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649679_1499163964_ma.jpg",
+      price: 155
+    },
+    {
+      id: 5 ,
+      catagory:'Rice',
+      name: "Buta Don",
+      detail: "ข้าวหน้าหมูชาชูซอสขิง" ,
+      photo: "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649914_1499164092_ma.jpg",
+      qty: 40,
+      price: 130
+    },
+    {
+      id: 6 ,
+      catagory:'Snacks',
+      name: "Karaage ไก่ทอดคาราเกะ",
+      detail: "ไก่ทอดคาราเกะ" ,
+      photo : "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649840_1499342394_ma.jpg", 
+      qty: 60,
+      price: 110
+    },
+    {
+      id: 7 ,
+      catagory:'Ramen',
+      name: "Red Special",
+      detail: "ราเมนเส้นเล็ก ในน้ำซุปกระดูกหมูและมิโซะแดง ท้อปปิ้งด้วยหมูชาชู เห็ดหูหนู ไข่ และสาหร่ายให้ความเผ็ดนิดๆกำลังดี" ,
+      photo : "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649681_1499163904_ma.jpg", 
+      qty: 40,
+      price: 300
+    },
+    {
+      id: 8 ,
+      catagory:'Ramen',
+      name: "Red Ajitama",
+      detail: "ราเมนเส้นเล็ก ในน้ำซุปกระดูกหมูและมิโซะแดง ท้อปปิ้งด้วยหมูชาชูเห็ดหูหนูและไข่ ให้ความเผ็ดนิดๆกำลังดี" ,
+      photo : "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649680_1499163963_ma.jpg", 
+      qty: 40,
+      price: 260
+    },
+    {
+      id: 9 ,
+      catagory:'Ramen',
+      name: "Black Ajitama",
+      detail: "ราเมนเส้นเล็กในน้ำซุปกระดูกหมูที่ผสมกระเทียมเจียว ท้อปปิ้งด้วยเห็ดหูหนู และหมูชาชู และไข่" ,
+      photo : "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649682_1499163904_ma.jpg", 
+      qty: 40,
+      price: 250
+    },
+    {
+      id: 10 ,
+      catagory:'Ramen',
+      name: "Black Special",
+      detail: "ราเมนเส้นเล็กในน้ำซุปกระดูกหมูที่ผสมกระเทียมเจียว ท้อปปิ้งด้วยเห็ดหูหนู และหมูชาชู ไข่ และสาหร่าย" ,
+      photo : "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649683_1499163903_ma.jpg", 
+      qty: 40,
+      price: 250
+    },
+    {
+      id: 11 ,
+      catagory:'Ramen',
+      name: "Gyoton Ajitama",
+      detail: "ราเมนเส้นเล็กในน้ำซุปกระดูกหมูและปลา ท้อปปิ้งด้วยหมูชาชูเนื้อนุ่ม ไข่ และหัวหอมซอย" ,
+      photo : "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649684_1499163867_ma.jpg", 
+      qty: 40,
+      price: 260
+    },
+    {
+      id: 12 ,
+      catagory:'Ramen',
+      name: "Tokyo Ajitama",
+      detail: "ราเมนเส้นเล็กในน้ำซุปไก่แบบใสสไตล์ต้นตำรับ ท้อปปิ้งด้วยเนื้ออกไก่ หน่อไม้ดองสไตล์ญี่ปุ่น และไข่" ,
+      photo : "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649686_1499163866_ma.jpg", 
+      qty: 30,
+      price: 250
+    },
+    {
+      id: 13 ,
+      catagory:'Ramen',
+      name: "Tokyo Special",
+      detail: "ราเมนเส้นเล็กในน้ำซุปไก่แบบใสสไตล์ต้นตำรับ ท้อปปิ้งด้วยเนื้ออกไก่ หมูชาชู หน่อไม้ดองสไตล์ญี่ปุ่น และสาหร่าย" ,
+      photo : "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649687_1499163833_ma.jpg", 
+      qty: 50,
+      price: 60
+    },
+    {
+      id: 14 ,
+      catagory:'Snacks',
+      name: "Gyoza",
+      detail: "เกี๊ยวซ่า" ,
+      photo : "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649841_1499413776_ma.jpg", 
+      qty: 30,
+      price: 110
+    },
+    {
+      id: 15 ,
+      catagory:'',
+      name: "Chashumori",
+      detail: "หมูชาชูซอสขิง" ,
+      photo : "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649845_1499163708_ma.jpg", 
+      qty: 30,
+      price: 160
+    },
+    {
+      id: 16 ,
+      catagory:'Snacks',
+      name: "Tofu Salad",
+      detail: "สลัดเต้าหู้" ,
+      photo : "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649844_1499342449_ma.jpg", 
+      qty: 40,
+      price: 130
+    },
+    {
+      id: 17 ,
+      catagory:'Snacks',
+      name: "Chuka Wakame",
+      detail: "ยำสาหร่ายญี่ปุ่น" ,
+      photo : "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649847_1499239815_ma.jpg", 
+      qty: 40,
+      price: 110
+    },
+    {
+      id: 18 ,
+      catagory:'Other',
+      name: "Otsumami Menma",
+      detail: "หน่อไม้ดองสไตล์ญี่ปุ่น" ,
+      photo : "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649846_1499239874_ma.jpg", 
+      qty: 40,
+      price: 150
+    },
+    {
+      id: 19 ,
+      catagory:'Rice',
+      name: "Karaage Curry Rice",
+      detail: "ข้าวหน้าแกงกะหรี่ไก่ทอด" ,
+      photo : "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649913_1499342385_ma.jpg", 
+      qty: 40,
+      price: 200
+    },
+    {
+      id: 20 ,
+      catagory:'Rice',
+      name: "Mentai Gohan",
+      detail: "ข้าวหน้าไข่ปลาเมนไทโกะ" ,
+      photo : "https://asia-public.foodpanda.com/dynamic/production/th/images/products/649/649918_1499239873_ma.jpg", 
+      qty: 40,
+      price: 110
+    }
+  ];
